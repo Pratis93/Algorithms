1df9d8508c2e35989988f68dc5e14c7af35463cf
Pratis93 <pawellak@gmail.com>
2018-04-23 14:29:20 +0200

    Lista Jednokierunkowa

---
+++ ListaJednoKierunkowa/ListaJednoKierunkowa/ListaJednoKierunkowa/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ListaJednoKierunkowa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnListJedno = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnListJedno
+            // 
+            this.btnListJedno.Location = new System.Drawing.Point(12, 88);
+            this.btnListJedno.Name = "btnListJedno";
+            this.btnListJedno.Size = new System.Drawing.Size(177, 30);
+            this.btnListJedno.TabIndex = 0;
+            this.btnListJedno.Text = "New Element";
+            this.btnListJedno.UseVisualStyleBackColor = true;
+            this.btnListJedno.Click += new System.EventHandler(this.btnListJedno_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(46, 4);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(37, 17);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Imię:";
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Location = new System.Drawing.Point(12, 35);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(71, 17);
+            this.lblSurname.TabIndex = 2;
+            this.lblSurname.Text = "Nazwisko:";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(40, 63);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(43, 17);
+            this.lblAge.TabIndex = 3;
+            this.lblAge.Text = "Wiek:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(89, 4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(89, 32);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(100, 22);
+            this.txtSurname.TabIndex = 5;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(89, 60);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 22);
+            this.txtAge.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 356);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblSurname);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnListJedno);
+            this.Name = "Form1";
+            this.Text = "Lista Jednokierunkowa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListJedno;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtAge;
+    }
+}
+
---
+++ ListaJednoKierunkowa/ListaJednoKierunkowa/ListaJednoKierunkowa/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaJednoKierunkowa
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Element
+        {
+            public Element Next { get; set; }
+            public String Value { get; set; }
+
+            public Element(String value)
+            {
+                Value = value;
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Element root = new Element("root");
+            Element first = new Element("1st");
+            root.Next = first;
+            Element second = new Element("2nd");
+            first.Next = second;
+            Element third = new Element("3rd");
+            second.Next = third;
+            Element fourth = new Element("4th");
+            third.Next = fourth;
+            // Output
+            Element element = root;
+            while (element != null)
+            {
+                MessageBox.Show(element.Value.ToString());
+                element = element.Next;
+            }
+
+        }
+
+        
+
+        
+
+    private void btnListJedno_Click(object sender, EventArgs e)
+        {
+
+                
+
+
+
+
+
+        }
+    }
+}
